e670635098921f1490265c15143bfc72f4f4750e
CM-Gimenez <75501546+CM-Gimenez@users.noreply.github.com>
2022-09-22 22:47:47 -0300

    routes

--- routes/api.php
+++ routes/api.php
@@ -19,18 +19,18 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 //CRUD de productos
-Route::apiResource('/productos','\App\Http\Controllers\ProductoController'::class );
+//Route::apiResource('/productos','\App\Http\Controllers\ProductoController'::class );
 
 //mostrar todos los productos pagiNADOS DE 10EN 10
-//Route::get("/productos",['\App\Http\Controllers\ProductoController', 'index']);
+Route::get("/productos",['\App\Http\Controllers\ProductoController', 'index']);
 //MOSTRAR UN PRODUCTO POR SU ID
-//Route::get("/productos/{producto}",['\App\Http\Controllers\ProductoController', 'show']);
+Route::get("/productos/{producto}",['\App\Http\Controllers\ProductoController', 'show']);
 //BUSCAR PRODUCTOS 
 //Borrar producto
-//Route::delete("/productos/{producto}",['\App\Http\Controllers\ProductoController', 'destroy']);
+Route::delete("/productos/{producto}",['\App\Http\Controllers\ProductoController', 'destroy']);
 //Crear un nuevo product
-//Route::post("/productos", ['\App\Http\Controllers\ProductoController','store']);
+Route::post("/productos", ['\App\Http\Controllers\ProductoController','store']);
 //modificar un producto
-//Route::put("/productos/{producto}", ['\App\Http\Controllers\ProductoController','update']);
+Route::put("/productos/{producto}", ['\App\Http\Controllers\ProductoController','update']);
 
 
